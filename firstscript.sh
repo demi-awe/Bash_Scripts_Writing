64ceab755783c556c5bb0d4e66b90e83f6e45ffb
Demi Awe <66763477+dawe3799@users.noreply.github.com>
2022-09-02 16:04:23 -0400

    Create firstscript.sh

---
+++ firstscript.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir scripts
+cd scripts
+touch HelloWorld.txt
+echo "Hello World, this is Aanu, if you are seeing this file, then you have successfully run your first script" > HelloWorld.txt
+cat HelloWorld.txt

384227bed1d88382cce7defea2aa57f1e301d0b9
Demi Awe <66763477+dawe3799@users.noreply.github.com>
2023-01-12 14:21:06 -0500

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,10 @@
+#Install Jenkins
+echo "Installing Jenkins ....."
+sudo apt install jenkins -y
+
+#start Jenkins Service
+#echo "Starting Jenkins...."
+#sudo systemctl ststus jenkins.service
+
+#Check Jenkins Status
+#echo "Jenkins status..."

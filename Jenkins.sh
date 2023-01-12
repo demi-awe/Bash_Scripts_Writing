ed34fac627a5c46c805b99bcb5f1d8522010d3dc
Demi Awe <66763477+dawe3799@users.noreply.github.com>
2023-01-12 14:26:33 -0500

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -4,7 +4,26 @@ sudo apt install jenkins -y
 
 #start Jenkins Service
 #echo "Starting Jenkins...."
-#sudo systemctl ststus jenkins.service
+#sudo systemctl status jenkins.service
 
 #Check Jenkins Status
 #echo "Jenkins status..."
+#sudo systemctl status jenkins
+
+#Allow firewall
+echo "Allow firewall..."
+sudo ufw allow 8080
+
+#Check new rules
+sudo ufw status
+echo "Checking new rules....."
+
+#if ufw is disabled
+sudo ufw allow OpenSSH
+sudo ufw enable -y
+
+sudo ufw allow 8080
+
+#View password
+echo "Jenkins adminpassword..."
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
